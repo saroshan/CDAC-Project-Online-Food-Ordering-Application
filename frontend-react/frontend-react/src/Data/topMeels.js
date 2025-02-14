5136c86de919c5fce43adb74fc7e855d160ccf0b
saroshan <roshansawant071@gmail.com>
2025-02-14 07:38:50 +0530

    frontend Mom's kitchen

---
+++ frontend-react/frontend-react/src/Data/topMeels.js
@@ -0,0 +1,58 @@
+export const topMeels=[
+    {
+        image:"https://media.istockphoto.com/id/873539518/photo/deep-fried-bread-spicy-chickpeas-curry-and-salad.jpg?s=612x612&w=0&k=20&c=S3YF4G2hCVQCccnjC0Q_Q4RXmR2vQmzDOcvdVwOjgLU=",
+        title:"Chole Bhature"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2021/06/28/14/06/curry-6371737_1280.jpg",
+        title:"Veg Thali"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2023/06/02/22/10/ai-generated-8036516_1280.png",
+        title:"Panner"
+    }
+    ,
+    {
+        image:"https://cdn.pixabay.com/photo/2012/07/09/07/48/eat-52075_1280.jpg",
+        title:"South Indian Food"
+    }
+    ,
+    {
+        image:"https://cdn.pixabay.com/photo/2016/03/27/22/38/cake-1284548_640.jpg",
+        title:"Cake"
+    }
+    ,
+    {
+        image:"https://cdn.pixabay.com/photo/2022/06/27/05/38/spices-7286739_1280.jpg",
+        title:"Biryani"
+    }
+    ,
+    {
+        image:"https://cdn.pixabay.com/photo/2012/07/09/07/16/thali-51996_1280.jpg",
+        title:'Thali'
+    }
+    ,
+    {
+        image:"https://images.pexels.com/photos/4331490/pexels-photo-4331490.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"North Indian Food"
+    }
+    ,
+    {
+        image:"https://cdn.pixabay.com/photo/2017/08/25/15/10/egg-roll-2680478_640.jpg",
+        title:"Rolls"
+    }
+    ,
+    {
+        image:"https://images.pexels.com/photos/2313686/pexels-photo-2313686.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"Chiken"
+    },
+    {
+        image:"https://images.pexels.com/photos/2764905/pexels-photo-2764905.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"Noodles"
+    },
+    {
+        image:"https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"Dosa"
+    },
+
+]
